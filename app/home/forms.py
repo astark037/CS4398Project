213931da1095f4eb9e439512ddb7b19808061e64
calebSmith163 <caleb.smith5591@gmail.com>
2017-03-26 07:52:32 +0000

    Added non-admin user ability to view and edit personal and payroll information

---
+++ app/home/forms.py
@@ -0,0 +1,55 @@
+# app/admin/forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, ValidationError, IntegerField, BooleanField, DecimalField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange, AnyOf
+from ..models import Employee, Payroll, Compensation
+
+class PersonalInfoForm(FlaskForm):
+    """
+    Form for admin to edit employee personal info
+    """
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=60)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=60)])
+    middle_name = StringField('Middle Name', validators=[Length(min=1, max=60)])
+    home_address = StringField('Home Address', validators=[DataRequired(), Length(max=60)])
+    mailing_address = StringField('Mailing Address', validators=[DataRequired(), Length(max=60)])
+    home_phone = StringField('Home Phone', validators=[Length(max=15)])
+    cell_phone = StringField('Cell Phone', validators=[DataRequired(), Length(max=15)])
+    submit = SubmitField('Submit')
+
+class PayrollForm(FlaskForm):
+    """
+    Form for admin to edit employee personal info
+    """
+    #eid = StringField('Employee ID', validators=[DataRequired(), ])
+    account_type = StringField('Account Type', validators=[DataRequired(), AnyOf(['Checking', 'Savings'])])
+    account_num = StringField('Account Number', validators=[DataRequired(), Length(min=9, max=9)])
+    routing_num = StringField('Routing Number', validators=[DataRequired(), Length(min=9, max=9)])
+    amount_withheld = IntegerField('Amount Withheld', validators=[ NumberRange(min=0)])
+    num_allowances = IntegerField('Number of Allowances', validators=[ NumberRange(min=0)])
+    claim_exemption = StringField('Claim Exemption', validators=[DataRequired(), AnyOf(['True', 'False', 'Yes', 'No'])])
+    submit = SubmitField('Submit')
+    
+    def validate_eid(self, field):
+        if Employee.query.filter_by(id=field.data).first() == None:
+            raise ValidationError('Employee ID not found.')
+        if Payroll.query.filter_by(eid=field.data).first():
+            raise ValidationError('Payroll info has already been entered for this employee.')
+
+class CompensationForm(FlaskForm):
+    """
+    Form for admin to edit employee compensation info
+    """
+    eid = StringField('Employee ID', validators=[DataRequired()])
+    pay_period = StringField('Pay Period', validators=[DataRequired()])
+    net_pay = DecimalField('Net Pay', validators=[DataRequired()])
+    gross_pay = DecimalField('Gross Pay', validators=[DataRequired()])
+    hourly_wage = DecimalField('Hourly Wage', validators=[DataRequired()])
+    hours_worked = DecimalField('Hours Worked', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def validate_eid(self, field):
+        if Employee.query.filter_by(id=field.data).first() == None:
+            raise ValidationError('Employee ID not found.')
--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,12 @@
 # app/home/views.py
 
-from flask import abort, render_template
+from flask import flash, abort, render_template, redirect, url_for
 from flask_login import current_user, login_required
 
 from . import home
+from forms import PersonalInfoForm, PayrollForm, CompensationForm
+from .. import db
+from ..models import Employee, Payroll, Compensation
 
 @home.route('/')
 def homepage():
@@ -19,6 +22,126 @@ def dashboard():
     Render the dashboard template on the /dashboard route
     """
     return render_template('home/dashboard.html', title="Dashboard")
+
+@home.route('/personalinfos/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_personalinfo(id):
+    """
+    Edit an employee's personal info
+    """
+
+    personalinfo = Employee.query.get_or_404(id)
+    form = PersonalInfoForm(obj=personalinfo)
+    if form.validate_on_submit():
+        personalinfo.first_name = form.first_name.data
+        personalinfo.last_name = form.last_name.data
+        personalinfo.middle_name = form.middle_name.data
+        personalinfo.email = form.email.data
+        personalinfo.home_address = form.home_address.data
+        personalinfo.mailing_address = form.mailing_address.data
+        personalinfo.home_phone = form.home_phone.data
+        personalinfo.cell_phone = form.cell_phone.data
+
+        db.session.commit()
+        flash('You have successfully edited the employee.')
+
+        # redirect to the employee page
+        return redirect(url_for('home.dashboard'))
+
+    form.first_name.data = personalinfo.first_name
+    form.last_name.data = personalinfo.last_name
+    form.middle_name.data = personalinfo.middle_name
+    form.email.data = personalinfo.email
+    form.home_address.data = personalinfo.home_address
+    form.mailing_address.data = personalinfo.mailing_address
+    form.home_phone.data = personalinfo.home_phone
+    form.cell_phone.data = personalinfo.cell_phone
+
+    return render_template('home/personalinfo.html', action="Edit",
+                           form=form,
+                           personalinfo=personalinfo, title="Edit Personal Info")
+
+
+@home.route('/payrolls')
+@login_required
+def list_payrolls():
+    """
+    List payroll info for this employee
+    """
+    payrolls = Payroll.query.filter_by(eid=current_user.id).all()
+    return render_template('home/payrolls.html',
+                           payrolls=payrolls, title='Payrolls')
+
+
+
+
+@home.route('/payrolls/add', methods=['GET', 'POST'])
+@login_required
+def add_payroll():
+    """
+    Add payroll info to the database
+    """
+
+
+    add_payroll = True
+
+    form = PayrollForm()
+    if form.validate_on_submit(): 
+        payroll = Payroll(account_type=form.account_type.data,
+                          account_num=form.account_num.data,
+                          routing_num=form.routing_num.data,
+                          amount_withheld=form.amount_withheld.data,
+                          num_allowances=form.num_allowances.data,
+                          claim_exemption=form.claim_exemption.data,
+                         eid=current_user.id)
+                          
+        db.session.add(payroll)
+        db.session.commit()
+        flash('You have successfully added new payroll info.')
+        # redirect to the payrolls page
+        return redirect(url_for('home.dashboard'))
+        
+    return render_template('home/payroll.html', add_payroll=add_payroll,
+                       form=form, title='Add Payroll')
+
+
+@home.route('/payrolls/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_payroll(id):
+    """
+    Edit payroll info for an employee
+    """
+
+    add_payroll = False
+
+    payroll = Payroll.query.get_or_404(id)
+    form = PayrollForm(obj=payroll)
+    if form.validate_on_submit():
+        payroll.account_type = form.account_type.data
+        payroll.account_num = form.account_num.data
+        payroll.routing_num = form.routing_num.data
+        payroll.amount_withheld = form.amount_withheld.data
+        payroll.num_allowances = form.num_allowances.data
+        payroll.claim_exemption = form.claim_exemption.data
+        db.session.add(payroll)
+        db.session.commit()
+        flash('You have successfully edited the payroll info.')
+
+        # redirect to the payrolls page
+        return redirect(url_for('home.dashboard'))
+
+    form.account_type.data = payroll.account_type
+    form.account_num.data = payroll.account_num
+    form.routing_num.data = payroll.routing_num
+    form.amount_withheld.data = payroll.amount_withheld
+    form.num_allowances.data = payroll.num_allowances
+    form.claim_exemption.data = payroll.claim_exemption
+    return render_template('home/payroll.html', add_payroll=add_payroll,
+                           form=form, title="Edit Payroll")
+
+
+
+
 
 @home.route('/admin/dashboard')
 @login_required
